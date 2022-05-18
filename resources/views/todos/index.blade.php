3c642d3fe66ce16ba0d4ec42af6701e09fcfe81c
Diego Ortiz Carmelino <dortiz@SEDACUSCO.COM>
2022-05-18 12:35:03 -0500

    1ra practica con Laravel desde la Oficina

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+
+@section('content')
+    <div class="container w-25 border p-4 mt-4">
+        <form>
+            <div class="mb-3">
+                <label for="title" class="form-label">Titulo de la Tarea</label>
+                <input type="text" class="form-control" name="title">
+           </div>
+            
+            <button type="submit" class="btn btn-primary">Crear nueva tarea</button>
+        </form>
+    </div>
+@endsection
